a440c78d2b176390f521865715926e976b41a414
Nick Ocampo <nick@wehatenick.com>
2017-10-18 19:28:43 -0700

    renamed receiv to cb for clarity

--- src/chess-board/Game.js
+++ src/chess-board/Game.js
@@ -1,5 +1,5 @@
-export function observe (receive) {
-  setInterval(() => receive([
+export function observe (cb) {
+  setInterval(() => cb([
     Math.floor(Math.random() * 8),
     Math.floor(Math.random() * 8)
   ]), 500);
