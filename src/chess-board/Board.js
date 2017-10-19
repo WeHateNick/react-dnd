3e773e8806787c4731331cdf75496a043aa7aa30
Nick Ocampo <nick@wehatenick.com>
2017-10-18 21:41:54 -0700

    got some dragging and dropping going

--- src/chess-board/Board.js
+++ src/chess-board/Board.js
@@ -1,59 +1,63 @@
 import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 import PropTypes from 'prop-types';
-import Square from './Square';
+import BoardSquare from './BoardSquare';
 import Knight from './Knight';
 import { moveKnight, canMoveKnight } from './Game';
 
-export default class Board extends Component {
+class Board extends Component {
   renderSquare(i) {
-      const x = i % 8;
-      const y = Math.floor(i / 8);
-      const black = (x + y) % 2 === 1;
+    const x = i % 8;
+    const y = Math.floor(i / 8);
 
-      const [knightX, knightY] = this.props.knightPosition;
-      const piece = (x === knightX && y === knightY) ?
-        <Knight /> :
-        null;
+    return (
+      <div 
+        key={i}
+        style={{ 
+          width: '12.5%', 
+          height: '12.5%'
+        }}
+      >
+        <BoardSquare x={x} y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
+      </div>
+    );
+  }
 
-      return (
-        <div 
-          key={i}
-          style={{ 
-            width: '12.5%', 
-            height: '12.5%'
-          }}
-          onClick={() => this.handleSquareClick(x, y)}
-          >
-          <Square black={black}>
-            {piece}
-          </Square>
-        </div>
-      );
-    }
-
-    render() {
-      const squares = [];
-      for (let i = 0; i < 64; i++) {
-        squares.push(this.renderSquare(i));
-      }
-
-      return (
-        <div className='Board'>
-          {squares}
-        </div>
-      );
-    }
-
-    handleSquareClick(toX, toY) {
-      if (canMoveKnight(toX, toY)) {
-        moveKnight(toX, toY);
-      }
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />
     }
   }
 
+  render() {
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
+    }
 
-  Board.propTypes = {
-    knightPosition: PropTypes.arrayOf(
-      PropTypes.number.isRequired
-    ).isRequired
-  };
+    return (
+      <div className='Board'>
+        {squares}
+      </div>
+    );
+  }
+
+  handleSquareClick(toX, toY) {
+    if (canMoveKnight(toX, toY)) {
+      moveKnight(toX, toY);
+    }
+  }
+}
+
+
+Board.propTypes = {
+  knightPosition: PropTypes.arrayOf(
+    PropTypes.number.isRequired
+  ).isRequired
+};
+
+export default DragDropContext(HTML5Backend)(Board);
--- src/chess-board/Knight.js
+++ src/chess-board/Knight.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { DragSource } from 'react-dnd'
+import { ItemTypes } from './Constants'
 
-export default class Knight extends Component {
-  render() {
-    return <span 
-    	style={{
-    		display: 'inline-flex',
-		    fontSize: '2em'
-    	}}>♘</span>;
+const knightSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
   }
 }
+
+class Knight extends Component {
+  render() {
+  	const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+    	<div 
+	    	style={{
+	    		display: 'inline-flex',
+			    fontSize: '2em',
+			    opacity: isDragging ? 0.5 : 1,
+			    cursor: 'move',
+	    	}}
+	    >♘</div>
+    );
+  }
+}
+
+Knight.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
+};
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);
